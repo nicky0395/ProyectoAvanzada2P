47e1fb89518fd30a76405ffb17d67747c00610a7
Oscar Fierro <35376722+Osk4r1@users.noreply.github.com>
2018-08-08 12:34:42 -0500

    Android Project
    Android Project

---
+++ Proyecto TercerP/src/main/java/com/espe/oscar/holamundo/MainActivity.java
@@ -0,0 +1,132 @@
+package com.espe.oscar.holamundo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintStream;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button botonNombre;
+    private TextView editText2;
+    private TextView editText;
+
+    private Context context = this;
+    private static final int SERVERPORT = 5000;
+    private static final String ADDRESS = "10.40.235.186";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        botonNombre = findViewById(R.id.button);
+        editText2 = findViewById(R.id.lblNombre);
+        editText = findViewById(R.id.txtNombre);
+
+        botonNombre.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //salida.setText(entrada.getText());
+                if(editText.getText().toString().length()>0){
+                    MyATaskCliente myATaskYW = new MyATaskCliente();
+                    myATaskYW.execute(editText.getText().toString());
+                }else{
+                    Toast.makeText(context, "Escriba \"frase\" o \"libro\" ", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+
+    class MyATaskCliente extends AsyncTask<String,Void,String>{
+
+        /**
+         * Ventana que bloqueara la pantalla del movil hasta recibir respuesta del servidor
+         * */
+        ProgressDialog progressDialog;
+
+        /**
+         * muestra una ventana emergente
+         * */
+        @Override
+        protected void onPreExecute()
+        {
+            super.onPreExecute();
+            progressDialog = new ProgressDialog(context);
+            progressDialog.setCanceledOnTouchOutside(false);
+            progressDialog.setTitle("Connecting to server");
+            progressDialog.setMessage("Please wait...");
+            progressDialog.show();
+        }
+
+        /**
+         * Se conecta al servidor y trata resultado
+         * */
+        @Override
+        protected String doInBackground(String... values){
+
+            try {
+                //Se conecta al servidor
+                InetAddress serverAddr = InetAddress.getByName(ADDRESS);
+                Log.i("I/TCP Client", "Connecting...");
+                Socket socket = new Socket(serverAddr, SERVERPORT);
+                Log.i("I/TCP Client", "Connected to server");
+
+                //envia peticion de cliente
+                Log.i("I/TCP Client", "Send data to server");
+                PrintStream output = new PrintStream(socket.getOutputStream());
+                String request = values[0];
+                output.println("Desde Android: "+request);
+
+                //recibe respuesta del servidor y formatea a String
+                Log.i("I/TCP Client", "Received data to server");
+                InputStream stream = socket.getInputStream();
+                byte[] lenBytes = new byte[256];
+                stream.read(lenBytes,0,256);
+                String received = new String(lenBytes,"UTF-8").trim();
+                Log.i("I/TCP Client", "Received " + received);
+                Log.i("I/TCP Client", "");
+                //cierra conexion
+                socket.close();
+                return received;
+            }catch (UnknownHostException ex) {
+                Log.e("E/TCP Client", "" + ex.getMessage());
+                return ex.getMessage();
+            } catch (IOException ex) {
+                Log.e("E/TCP Client", "" + ex.getMessage());
+                return ex.getMessage();
+            }
+        }
+
+        /**
+         * Oculta ventana emergente y muestra resultado en pantalla
+         * */
+        @Override
+        protected void onPostExecute(String value){
+            progressDialog.dismiss();
+            editText2.setText(value);
+        }
+    }
+
+
+}
